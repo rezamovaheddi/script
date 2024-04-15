998fc9db4ca01a0f9354d9e423a9013235321529
rezamovaheddi <rmwhdy8@gamil.com>
2024-04-15 14:51:23 +0330

    arryes

---
+++ learning/script.js
@@ -0,0 +1,39 @@
+// const massMark = 95;
+// const highMark = 1.88;
+
+// const massJhon = 85;
+// const highJhon = 1.76;
+
+// const BMImark = massMark / highMark ** 2;
+
+// const BMIjhon = massJhon / (highJhon * highJhon);
+
+// if (BMIjhon > BMImark) {
+//     console.log(`${BMIjhon} is lorger`);
+// } else {
+//     console.log(`${BMImark} is larger`);
+// }
+
+
+
+
+
+// remove arry after last index with pop method 
+let arry = ['apple', 'banana', true, 1386]
+
+console.log(arry.pop());
+console.log(arry);
+
+// add last index with push method 
+
+let arr = ['apple', 'banana', true, 1386];
+arr.push(1386);
+console.log(arr);
+// remove first index arrye with method shift
+const ar = [true, 122, 'banana', 'appp'];
+console.log(ar.shift('ww'));
+console.log(ar);
+// add first index with unshift method 
+let a = ['apple', 'banana', true, 1386];
+console.log(a.unshift('reza'));
+console.log(a);
