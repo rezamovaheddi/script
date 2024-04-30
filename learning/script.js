346264f8fcdbfb4c438a4705362ce67fae538aac
rezamovaheddi <rmwhdy8@gamil.com>
2024-04-30 15:51:28 +0330

    func

--- learning/script.js
+++ learning/script.js
@@ -1,4 +1,6 @@
-function sumValues(from, text = 'Iran') {
-    from = from || 'empty'
+function num(num1 = 2, num2 = 2) {
+    let result = num1 + num2
+    console.log(result);
 }
-sumValues()
+
+num()
