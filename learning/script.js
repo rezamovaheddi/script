5b2e4f33cff92e976c18116dc87c3a856d9cd5f0
rezamovaheddi <rmwhdy8@gamil.com>
2024-04-30 15:43:18 +0330

    func

--- learning/script.js
+++ learning/script.js
@@ -1,19 +1,4 @@
-const hasD = false; // true
-const hasGood = true; // false
-
-// if (hasD && hasGood) {
-//     console.log("sarah can drive ");
-// } else {
-//     console.log("sarah cant drive");
-// }
-
-// if (hasD || hasGood) {
-//     console.log("sarah can drive car")
-// } else {
-//     console.log("sarah cant drive cars");
-// }
-if (!hasD || !hasGood) {
-    console.log("sarah has lisenses drive car");
-} else {
-    console.log("cant drive car");
+function sumValues(from, text = 'Iran') {
+    from = from || 'empty'
 }
+sumValues()
