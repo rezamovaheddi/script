9e2aed6d352975a57c1b23b301fa79a4e1fd981c
Reza <108584275+rezamovaheddi@users.noreply.github.com>
2024-05-02 19:10:12 +0330

    Update script.js

--- learning/script.js
+++ learning/script.js
@@ -1,3 +1,9 @@
 const bil = 40;
 let tip = bil <= 300 && bil >= 50 ? bil * 0.15 : bil * 0.2;
 console.log(`the bill is ${bil} and ${tip} and jameshon ${tip + bil}`);
+
+if(bil <= 300 && bil >= 50){
+  console.log(bil * 0.15);
+}else{
+  console.log(bil * 0.2)
+}
