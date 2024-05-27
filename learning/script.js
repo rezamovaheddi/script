0e274d6ffc8cafafcd72500468326522c5a1f268
rezamovaheddi <rmwhdy8@gamil.com>
2024-05-27 13:38:48 +0330

    instaling node

--- learning/script.js
+++ learning/script.js
@@ -1,19 +1,22 @@
-const userNumbers = []; /// 4
+const UserList = [{
+    name: 'reza',
+    family: 'movahedi',
+    age: 17,
+    city: 'Tehran',
+}, {
+    name: 'ali',
+    family: 'sajadi',
+    age: 45,
+    city: 'Babol',
 
-let counterNumber = 0;
-// [35, 84, 13]
-let sum = 0;
+}, {
+    name: 'Mohammad',
+    family: 'saeedinia',
+    age: 35,
+    city: 'Tehran'
 
-while (counterNumber != -1) {
-    counterNumber = Number(prompt("Enter your number \n get -1 if tou dont eny number"));
-    if (counterNumber != -1) {
-        userNumbers.push(counterNumber)
-    }
-}
+}]
 
-for (let i = 0; i < userNumbers.length; i++) {
-    sum += userNumbers[i] // sum => 35 + 84 + 13 >> 132
-}
-console.log(userNumbers);
-console.log(sum);
-console.log(`Avbet => ${sum / userNumbers.length}`);
+UserList.forEach((user) => {
+    console.log(user.name);
+})
